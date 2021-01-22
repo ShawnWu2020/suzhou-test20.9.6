93ca846e703364aaad19d596ea0edf657f99cc93
wyy <wyy@bjpowernode.com>
2021-01-22 22:54:25 +0800

    2021/1/22

--- src/com/bjpowernode/domian/Person.java
+++ src/com/bjpowernode/domian/Person.java
@@ -10,12 +10,14 @@ public class Person {
     private String address;
     private String email;
     private String test1;
-    private String test2;
+    private String test8;
     private String test3;
     private String test4;
 
     private String test6;
 
     private String test5;
+
+    private String test7;
 
 }
