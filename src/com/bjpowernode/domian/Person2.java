692bfbc00e6d2c84d0fde78af82f3a0cf348ae9a
wyy <wyy@bjpowernode.com>
2021-05-07 17:46:21 +0800

    person 2

---
+++ src/com/bjpowernode/domian/Person2.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.domian;
+
+/**
+ * ClassName:workspaceForGit01
+ * Package:com.bjpowernode.domian
+ *
+ * @author:Beck
+ * @date:2021-05-07 17:45
+ */
+public class Person2 {
+}
